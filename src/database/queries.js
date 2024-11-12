3bf520083c8a4a4f31fd1735f320938eba39fa4b
Linda M De Marco <maribeldemarco@gmail.com>
2024-11-12 20:26:03 -0300

    se agrega archivo repository con funcionalidad de crear producto

--- src/database/queries.js
+++ src/database/queries.js
@@ -1,4 +1,13 @@
 import { database } from "../config.js"
 
 // Queries para la base de datos.
-export default {}
+const queries = {
+    createProduct: `
+        INSERT INTO Productos (ProveedorID, CategoriaSubcategoriaID, Nombre, Marca, Stock, Perece, Fecha_Vencimiento)
+        OUTPUT INSERTED.*
+        VALUES (@ProveedorID, @CategoriaSubcategoriaID, @Nombre, @Marca, @Stock, @Perece, @Fecha_Vencimiento)
+    `
+};
+
+
+export default queries;
