c1d430e111c313422097180c9f8110b323199f60
Christian Gabor <chrisgabor95@gmail.com>
2024-11-16 20:48:04 -0300

    cors agregado

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from  'express';
 import { expressConfig } from './src/config.js';
 import productosReadRoutes from './src/routes/productosRead.routes.js';
+import cors from 'cors';
 
 const app = express();
 
 app.set('port', expressConfig.port)
 app.set('host', expressConfig.host);
 
+app.use(cors())
 app.use(productosReadRoutes)
 
 app.get('/', (req, res) => {
