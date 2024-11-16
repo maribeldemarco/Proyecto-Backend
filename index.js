9d7375104d2a00a26c4f4b245c55988e604385f3
guadalupe sirio <guadalupe.sirio@gmail.com>
2024-11-16 13:32:17 -0300

    Add operation Delete of CRUD

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from  'express';
 import { expressConfig } from './src/config.js';
+import productosDeleteRoute from './src/routes/productosDelete.route.js';
 
 const app = express();
 
 app.set('port', expressConfig.port)
 app.set('host', expressConfig.host);
 
+app.use(productosDeleteRoute)
 
 app.get('/', (req, res) => {
   res.send('Raiz del servidor')
