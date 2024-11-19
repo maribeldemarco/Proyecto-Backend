bec9863ab962915c1725a48f3a65def67591345f
Norberto Juarez <norbertorauloscar@gmail.com>
2024-11-18 21:31:55 -0300

    CORS

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from  'express';
 import { expressConfig } from './src/config.js';
 import productosUpdateRoutes from './src/routes/productosUpdate.routes.js';
+import cors from 'cors';
 
 const app = express();
 
 app.set('port', expressConfig.port)
 app.set('host', expressConfig.host);
+
+// Configurar CORS
+app.use(cors({
+  origin: 'http://localhost:4200', // Permitir solo el origen del frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Cabeceras permitidas
+}));
 
 app.use(express.json());
 
--- src/services/productosUpdate.service.js
+++ src/services/productosUpdate.service.js
@@ -1,6 +1,7 @@
 import { updateProductoRepository } from "../repository/productosUpdate.repository.js";
 
 const updateProductoService = (id, producto)=>{
+    console.log("estoy en el service");
     return updateProductoRepository(id, producto);
 };
 
